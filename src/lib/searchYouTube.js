6b6a725c95bca83a1943311cb87f644002328d41
fionamiaojing <fionamiaojing@gmail.com>
2018-05-09 20:04:04 -0700

    commit 1

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      q: query,
+      maxResults: 5,
+      key: window.YOUTUBE_API_KEY,
+    },
+    success: function(data) {
+      console.log(data)
+    },
+    failure: function(data) {
+      
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
